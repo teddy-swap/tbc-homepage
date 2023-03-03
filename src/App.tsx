e34f9372810793b127375e26ffecedcbf3d13c29
Alphonzo Escolar <phonz.dev@gmail.com>
2023-03-03 18:46:00 +0800

    Implement first and second sections' responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -52,102 +52,134 @@ function App() {
   }, []);
 
   return (
-    <div className="App w-[100vw] h-[100vh] pb-6">
-      <main className="container mx-auto relative">
-        <div className="bg-mobile-main bg-cover bg-center">
-          <header className="pt-4 px-5 flex">
-            <div className="flex justify-start w-[90px] lg:justify-start"><img src="teddy-logo.svg" alt="logo" className="w-[200px]" /></div>
+    <div>
+      <main className="App w-[100vw] h-[100vh] pb-6">
+
+        <div className="bg-mobile-main lg:bg-main bg-cover bg-center h-[100vh] relative outline outline-1">
+          {/* HEADER */}
+          <header className="pt-10 lg:pt-[30px] 2xl:pt-[58px] px-5 md:px-16">
+            <div className="flex flex-col items-center md:flex-row md:justify-between max-w-[1700px] m-auto">
+              <div className="w-[150px] md:w-[150px] xl:w-[180px] 2xl:w-[210px]"><img src="teddy-logo.svg" alt="logo" /></div>
+              <ul className="flex gap-5 mt-6">
+                <li className="w-[25px]"><a href="https://twitter.com/TeddySwap"><img src="twitter.svg" alt="twitter icon" /></a></li>
+                <li className="w-[25px]"><a href="https://t.me/teddyswap"><img src="telegram.svg" alt="telegram icon" /></a></li>
+                <li className="w-[25px]"><a href="https://discord.gg/GRvcAnqtZG"><img src="discord.svg" alt="discord icon" /></a></li>
+                <li className="w-[25px]"><a href="https://medium.com/@TeddySwapDEX"><img src="medium.svg" alt="medium icon" /></a></li>
+              </ul>
+            </div>
           </header>
-          <section className="mt-24">
-            <div className="w-[280px] m-auto">
-              <img src="teddybearclub-logo.png" alt="teddy bear club logo" />
+
+          {/* SECTION ONE */}
+          <section className="w-[300px] md:w-[500px] lg:w-[700px] mt-10 md:mt-[120px] lg:mt-[61px] 2xl:mt-[20px] lg:flex lg:justify-center 2xl:w-[1500px] m-auto 3xl:justify-start">
+            <div className="lg:order-2 md:self-center">
+
+              <div className="lg:w-[400px] 2xl:w-[640px] m-auto">
+                <img src="teddybearclub-logo.png" alt="teddy bear club logo" />
+              </div>
+
+              <h2 className="font-medium lg:w-[310px] 2xl:w-[603px] mt-10 text-center lg:text-left lg:text-base 2xl:text-[28px] 2xl:leading-[40px] text-white m-auto">
+                The TeddySwap Initial NFT Offering provides TEDY tokens, boosts yield farming, and more on the TeddySwap DEX.
+              </h2>
             </div>
-            <h2 className="w-[210px] mt-10 text-center text-sm text-white m-auto">
-              The TeddySwap Initial NFT Offering provides TEDY tokens, boosts yield farming, and more on the TeddySwap DEX.
-            </h2>
-            <div className="mt-8 w-[280px] m-auto">
-              <img src="teddy-smirk.png" alt="A cartoon teddy bear smirking" />
+
+            <div className="mt-8 absolute md:relative bottom-0 md:mt-0 w-[280px] md:w-[420px] 2xl:w-[643px] 2xl:ml-[45px] m-auto">
+              <img src="teddy-smirk.png" alt="A cartoon teddy bear smirking" />  
             </div>
+
           </section>
+
+          <div className="hidden xl:block w-full absolute bottom-0">
+            <img src="wave2.png" alt="wave design" className="w-full" />
+          </div>
+    
         </div>
-
+        {/* SECTION TWO */}
         <section className="bg-aztec pt-8">
-          <h3 className="text-gold-sand text-center">Mint Price: 150 ADA</h3>
+          <div className="w-[300px] md:w-[500px] m-auto lg:w-[700px] 3xl:w-[1200px]">
+            <h3 className="text-gold-sand text-center xl:hidden">Mint Price: 150 ADA</h3>
 
-          <div className="w-[280px] m-auto mt-5">
-            <h4 className="text-gold-sand text-[10px]">Each NFT includes</h4>
-            <ul className="text-gold-sand flex flex-col gap-4 mt-2">
-              <li className="bg-firefly text-center text-[22px] rounded py-5 font-bold w-[260px]">
-                Up to 30,000 TEDY
-              </li>
-              <li className="bg-firefly text-center text-[22px] rounded py-5 font-bold w-[260px] self-end">
-                +3% FISO Rewards <span className="text-[8px]">+up to 20%</span>
-              </li>
-              <li className="bg-firefly text-center text-[22px] rounded py-5 font-bold w-[260px]">
-                +2% LBE Bonus <span className="text-[8px]">*Up to 20%</span>
-              </li>
-            </ul>
+            <div className="mt-5 2xl:mt-14">
+            
+              <h4 className="text-gold-sand font-medium text-[12px] xl:text-[30px]">Each NFT includes</h4>
+
+              <ul className="text-gold-sand flex flex-col gap-4 3xl:gap-[46px] mt-2 2xl:mt-4">
+                <li className="bg-firefly text-center text-[22px] md:text-[30px] xl:text-[80px] rounded 3xl:rounded-[25px] py-5 md:py-7 3xl:py-[66px] font-bold w-[260px] md:w-[360px] 3xl:w-[1101px]">
+                  Up to 30,000 TEDY
+                </li>
+                <li className="bg-firefly text-center text-[22px] md:text-[30px] 3xl:text-[80px] rounded 3xl:rounded-[25px] py-5 md:py-7 3xl:py-[66px] font-bold w-[260px] md:w-[360px] 3xl:w-[1101px] self-end">
+                  +3% FISO Rewards <span className="text-[8px] md:text-[12px] 3xl:text-[16px]">+up to 20%</span>
+                </li>
+                <li className="bg-firefly text-center text-[22px] md:text-[30px] 3xl:text-[80px] rounded 3xl:rounded-[25px] py-5 md:py-7 3xl:py-[66px] font-bold w-[260px] md:w-[360px] 3xl:w-[1101px]">
+                  +2% LBE Bonus <span className="text-[8px] md:text-[12px] 3xl:text-[16px]">*Up to 20%</span>
+                </li>
+              </ul>
+            </div>
+            <div className="mt-16 xl:mt-[232px] xl:flex items-center justify-between gap-40">
+              <div className="text-white text-justify order-2 xl:w-[514px] font-bold xl:text-[28px]">
+                <p>
+                  The Teddy Bears Club is a limited NFT collection that provides utility to users of TeddySwap, the coziest stable-coin DEX on Cardano. Join the Teddy Bears Club and earn TEDY tokens.
+                </p>
+                <p className="mt-6">
+                  Members of the The Teddy Bears Club will receive 20,00 to 30,00 TEDY tokens  To learn more, check out our medium.
+                </p>
+              </div>
+              <ul className="hidden xl:grid grid-cols-1 xl:grid-cols-2 order-1 m-auto gap-8 xl:gap-10 mt-20 xl:mt-0">
+                <li className="order-2"><img src="teddy-1.png" alt="" /></li>
+                <li className="order-3"><img src="teddy-2.png" alt="" /></li>
+                <li className="order-1"><img src="teddy-3.png" alt="" /></li>
+                <li className="order-4"><img src="teddy-4.png" alt="" /></li>
+              </ul>
+            </div>
           </div>
-
-          <div className="mt-14 w-[220px] m-auto">
-            <p className="font-bold text-white text-[12px] ">
-              The Teddy Bears Club is a limited NFT collection that provides utility to users of TeddySwap, the coziest stable-coin DEX on Cardano. Join the Teddy Bears Club and earn TEDY tokens.
-            </p>
-            <p className="font-bold text-white text-[12px] mt-4">
-            Members of the The Teddy Bears Club will receive 20,00 to 30,00 TEDY tokens  To learn more, check out our medium.
-            </p>
-          </div>
-
-          <ul className="flex flex-col w-[200px] m-auto mt-10 gap-5">
-            <li className="order-2"><img src="teddy-1.png" alt="" /></li>
-            <li className="order-3"><img src="teddy-2.png" alt="" /></li>
-            <li className="order-1"><img src="teddy-3.png" alt="" /></li>
-            <li className="order-4"><img src="teddy-4.png" alt="" /></li>
-          </ul>
         </section>
 
-        <section className="pt-4 bg-aztec">
-          <h2 className="text-[#66A7F2] lg:text-[25px] text-[15px] font-bold">Explorer</h2>
-          <div className="my-4 flex w-full flex-col md:flex-row">
-            <div className="flex w-full">
+        <section className="pt-4 bg-aztec pt-8">
+          <div className="w-[300px] m-auto">
+            <h2 className="text-gold-sand lg:text-[25px] text-[15px] font-bold">Explorer</h2>
+
+            <div className="my-4 flex w-full flex-col md:flex-row">
+              <div className="flex w-full">
+                <Pagination size="medium" variant="outlined" page={page} count={Math.ceil(Number(bears?.filter(b => b.name.indexOf(search) !== -1 || search === '').length) / ASSETS_PER_PAGE)} sx={{ color: 'white' }} onChange={(e, v) => setPage(v)} />
+              </div>
+              <div className="flex w-full justify-end md:mt-0 mt-6">
+                <TextField className="w-full md:w-auto" id="outlined-basic" label="Search" variant="outlined" size="small" InputProps={{ endAdornment: <Search /> }} value={search} onChange={(e) => setSearch(e.target.value)} />
+              </div>
+            </div>
+
+            <div className="mt-4 grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 grid-cols-1 gap-12">
+              {bears?.filter(b => b.name.indexOf(search) !== -1 || search === '').slice((page - 1) * ASSETS_PER_PAGE, ((page - 1) * ASSETS_PER_PAGE) + ASSETS_PER_PAGE).map((a, i) => {
+                return (
+                  <Card sx={{ background: '#1a282e' }} className="!rounded-md p-6" elevation={6} key={i}>
+                    <div className="relative">
+                      <Image
+                        src={`https://teddyswap.infura-ipfs.io/ipfs/${a.url}`}
+                        preview={true}
+                        fallback="/t3.webp"
+                        placeholder={<div className="absolute w-full h-full top-0 left-0 grid justify-center content-center bg-blue-500 opacity-80"><SyncIcon className="animate-spin" /></div>}
+                      />
+                    </div>
+                    <CardHeader
+                      avatar={
+                        <Avatar sx={{ bgcolor: 'rgb(102 167 242)', color: "#FFF" }} aria-label="recipe">
+                          {(ASSETS_PER_PAGE * (page - 1)) + i + 1}
+                        </Avatar>
+                      }
+                      title={a.name}
+                      subheader={`${getRankTokens(parseInt(a.rarityRank))} $TEDY Tokens + 10% Bonus`}
+                      titleTypographyProps={{
+                        className: 'text-white'
+                      }}
+                    />
+                  </Card>
+                );
+              })}
+            </div>
+            <div className="my-4 hidden xl:block">
               <Pagination size="large" variant="outlined" page={page} count={Math.ceil(Number(bears?.filter(b => b.name.indexOf(search) !== -1 || search === '').length) / ASSETS_PER_PAGE)} sx={{ color: 'white' }} onChange={(e, v) => setPage(v)} />
             </div>
-            <div className="flex w-full justify-end md:mt-0 mt-4">
-              <TextField className="w-full md:w-auto" id="outlined-basic" label="Search" variant="outlined" size="small" InputProps={{ endAdornment: <Search /> }} value={search} onChange={(e) => setSearch(e.target.value)} />
-            </div>
-          </div>
-          <div className="mt-4 grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 grid-cols-1 gap-12">
-            {bears?.filter(b => b.name.indexOf(search) !== -1 || search === '').slice((page - 1) * ASSETS_PER_PAGE, ((page - 1) * ASSETS_PER_PAGE) + ASSETS_PER_PAGE).map((a, i) => {
-              return (
-                <Card sx={{ background: '#1a282e' }} className="!rounded-md p-6" elevation={6} key={i}>
-                  <div className="relative">
-                    <Image
-                      src={`https://teddyswap.infura-ipfs.io/ipfs/${a.url}`}
-                      preview={true}
-                      fallback="/t3.webp"
-                      placeholder={<div className="absolute w-full h-full top-0 left-0 grid justify-center content-center bg-blue-500 opacity-80"><SyncIcon className="animate-spin" /></div>}
-                    />
-                  </div>
-                  <CardHeader
-                    avatar={
-                      <Avatar sx={{ bgcolor: 'rgb(102 167 242)', color: "#FFF" }} aria-label="recipe">
-                        {(ASSETS_PER_PAGE * (page - 1)) + i + 1}
-                      </Avatar>
-                    }
-                    title={a.name}
-                    subheader={`${getRankTokens(parseInt(a.rarityRank))} $TEDY Tokens + 10% Bonus`}
-                    titleTypographyProps={{
-                      className: 'text-white'
-                    }}
-                  />
-                </Card>
-              );
-            })}
-          </div>
-          <div className="my-4">
-            <Pagination size="large" variant="outlined" page={page} count={Math.ceil(Number(bears?.filter(b => b.name.indexOf(search) !== -1 || search === '').length) / ASSETS_PER_PAGE)} sx={{ color: 'white' }} onChange={(e, v) => setPage(v)} />
           </div>
         </section>
+        
       </main>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,tsx}"],
   theme: {
+    screens: {
+      'md': "768px",
+      'lg': "1024px",
+      'xl': "1280px",
+      '2xl': "1536px",
+      '3xl': "1920px"
+    },
     extend: {
       colors: {
         'teddy-active': "#46B58D",
@@ -13,7 +20,12 @@ module.exports = {
       },
       backgroundImage: {
         'main': "url('/main-bg.png')",
-        'mobile-main': "url('/mobile-main-bg.png')"
+        'wave': "url('/wave2.png')",
+        'teddy': "url('/teddy-smirk.png')",
+        'mobile-main': "url('/mobile-main-bg.png')",
+      },
+      backgroundPosition: {
+        'left-bottom': "left bottom",
       }
     },
   },
