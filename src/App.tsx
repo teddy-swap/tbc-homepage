d62cb3315ca4d04d3d23171fce4f5bb3cbe0b55e
Alphonzo Escolar <phonz.dev@gmail.com>
2023-03-08 14:37:33 +0800

    Layout improvements

--- src/App.tsx
+++ src/App.tsx
@@ -52,9 +52,9 @@ function App() {
     <main className="App w-[100vw] h-[100vh] pb-14 bg-aztec">
       {/* SECTION ONE */}
       <section id="section-1" className="bg-mobile-main lg:bg-main bg-cover bg-center h-[100vh] relative">
-        <header className="pt-10 xs:pt-14 lg:pt-[50px] 2xl:pt-[20px] 4xl:pt-[20px] px-5 md:px-16 lg:absolute lg:z-[2] lg:w-full">
+        <header className="pt-8 xs:pt-14 lg:pt-[30px] px-5 md:px-16 absolute z-[10] w-full">
           <div className="flex flex-col items-center md:flex-row md:justify-between max-w-[1700px] m-auto">
-            <div className="w-[150px] md:w-[150px] xl:w-[180px] 2xl:w-[210px]"><img src="teddy-logo.svg" alt="logo" /></div>
+            <div className="w-[150px] md:w-[150px] xl:w-[180px] 2xl:w-[210px]"><a href="/"><img src="teddy-logo.svg" alt="logo" /></a></div>
             <ul className="flex gap-5 mt-6 md:mt-0">
               <li className="w-[25px]"><a target="_blank" href="https://twitter.com/TeddySwap" rel="noreferrer"><img src="twitter.svg" alt="twitter icon" /></a></li>
               <li className="w-[25px]"><a target="_blank" href="https://t.me/teddyswap" rel="noreferrer"><img src="telegram.svg" alt="telegram icon" /></a></li>
@@ -64,19 +64,19 @@ function App() {
           </div>
         </header>
 
-        <div className="z-[2] lg:pt-[18.38%] w-full lg:absolute bottom-0 xl:bg-transparent">
-          <div className="z-[1] px-6 lg:p-0 max-w-[500px] lg:max-w-[900px] 2xl:max-w-[1200px] 4xl:max-w-[1700] m-auto lg:flex lg:absolute bottom-[calc(155px+13vw)] xl:bottom-[calc(20px+13vw)] 2xl:bottom-[calc(10px+13vw)] 4xl:bottom-[calc(18px+13vw)] left-0 right-0 justify-center mt-10 xs:mt-20 md:mt-40 2xl:mt-0">
+        <div className="z-[2] lg:pt-[18.38%] w-full lg:absolute lg:bottom-0 xl:bg-transparent">
+          <div className="px-6 lg:p-0 max-w-[500px] lg:max-w-[900px] 2xl:max-w-[1200px] 4xl:max-w-[1700] m-auto lg:flex lg:absolute bottom-[calc(155px+13vw)] xl:bottom-[calc(20px+13vw)] 2xl:bottom-[calc(10px+13vw)] 4xl:bottom-[calc(18px+13vw)] left-0 right-0 justify-center pt-[calc(70px+15vw)] xs:pt-[calc(130px+15vw)]">
             <div className="lg:order-2 lg:self-center lg:w-[400px] 3xl:w-[640px] lg:mt-14">
               <img src="teddybearclub-logo.png" alt="teddy bear club logo" />
-              <h2 className="font-medium px-4 lg:w-[90%] mt-2 sm:mt-6 text-justify lg:text-left lg:text-base 3xl:text-[24px] 3xl:leading-[35px] text-white m-auto">
+              <h2 className="font-medium px-4 lg:w-[90%] mt-2 sm:mt-6 text-center lg:text-left lg:text-base 3xl:text-[24px] 3xl:leading-[35px] text-white m-auto">
                 The TeddySwap Initial NFT Offering is your gateway to acquiring TEDY tokens, boosting your yield farming profits, and accessing exclusive benefits that are only available to members of the Teddy Bears Club NFT collection. 
               </h2>
             </div>
-            <div className="mt-8 lg:mt-0 absolute left-0 right-0 m-auto lg:relative bottom-0 w-[280px] md:w-[420px] lg:w-[413px] 3xl:w-[643px] 4xl:w-1/2 2xl:ml-[45px] xl:mx-0">
+            <div className="mt-8 lg:mt-0 absolute z-[10] left-0 right-0 m-auto lg:relative bottom-0 w-[280px] md:w-[420px] lg:w-[413px] 3xl:w-[643px] 4xl:w-1/2 2xl:ml-[45px] xl:mx-0">
               <img className="w-full" src="teddy-smirk.png" alt="A cartoon teddy bear smirking" />
             </div>
           </div>
-          <div className="hidden lg:block bg-wave bg-contain bg-no-repeat w-full pt-[18.38%] absolute bottom-[calc(311px-15vw)] xl:bottom-0 z-[2]"></div>
+          <div className="hidden lg:block bg-wave bg-contain bg-no-repeat w-full pt-[18.38%] absolute bottom-[calc(311px-15vw)] xl:bottom-[-1px] z-[11]"></div>
           <div className="hidden lg:block h-[calc(315px-15vw)] bg-aztec xl:hidden"></div>
         </div>
       </section>
@@ -106,16 +106,16 @@ function App() {
                 <h4 className="text-gold-sand font-medium text-[14px] lg:text-[18px] xl:text-[30px]">Each NFT includes</h4>
                 <ul className="text-gold-sand flex flex-col gap-4 sm:gap-6 xl:gap-[35px] 3xl:gap-[46px] mt-2 2xl:mt-4">
                   <li className="bg-firefly text-center text-[22px] md:text-[30px] xl:text-[50px] 4xl:text-[60px] rounded sm:rounded-[8px] lg:rounded-[15px] 4xl:rounded-[25px] py-5 md:py-7 xl:py-10 4xl:py-[66px] font-bold w-[90%] 4xl:w-[90%]">
-                    Up to 30,000 TEDY
+                    Up to 30,800 TEDY
                   </li>
                   <li className="bg-firefly text-center text-[22px] md:text-[30px] xl:text-[50px] 4xl:text-[60px] sm:rounded-[8px] lg:rounded-[15px] rounded 4xl:rounded-[25px] xl:py-10 py-5 md:py-7 4xl:py-[66px] font-bold w-[90%] 4xl:w-[90%] self-end">
-                    +5% FISO Rewards <span className="text-[8px] md:text-[12px] 3xl:text-[16px]">*per NFT</span>
+                    +5% FISO Rewards
                   </li>
                   <li className="bg-firefly text-center text-[22px] md:text-[30px] xl:text-[50px] 4xl:text-[60px] sm:rounded-[8px] lg:rounded-[15px] rounded 4xl:rounded-[25px] xl:py-10 py-5 md:py-7 4xl:py-[66px] font-bold w-[90%] 4xl:w-[90%]">
-                    +1% LBE Bonus <span className="text-[8px] md:text-[12px] 3xl:text-[16px]">*per NFT</span>
+                    +1% LBE Bonus
                   </li>
                   <li className="bg-firefly text-center text-[22px] md:text-[30px] xl:text-[50px] 4xl:text-[60px] sm:rounded-[8px] lg:rounded-[15px] rounded 4xl:rounded-[25px] xl:py-10 py-5 md:py-7 4xl:py-[66px] font-bold w-[90%] 4xl:w-[90%] self-end">
-                    +1% on Yield Farming <span className="text-[8px] md:text-[12px] 3xl:text-[16px]">*per NFT</span>
+                    +1% on Yield Farming
                   </li>
                 </ul>
               </div>
@@ -125,7 +125,7 @@ function App() {
                     The Teddy Bears Club is a limited NFT collection that provides advantages to TeddySwap users on Cardano. You can join the Teddy Bears Club and obtain TEDY tokens.
                   </p>
                   <p className="mt-6">
-                  As a member of the Teddy Bears Club in round 1, you have the opportunity to receive 10,500 up to 30,000 TEDY tokens. To learn more, check out our <a target="_blank" className="font-black underline" href="https://docs.teddyswap.org/articles/teddy-bears-club-minting-utility-and-launch-date" rel="noreferrer">article</a>.
+                  As a member of the Teddy Bears Club in round 1, you have the opportunity to receive 10,500 up to 30,800 TEDY tokens. To learn more, check out our <a target="_blank" className="font-black underline" href="https://docs.teddyswap.org/articles/teddy-bears-club-minting-utility-and-launch-date" rel="noreferrer">article</a>.
                   </p>
                 </div>
                 <ul className="hidden m-0 grow sm:grid grid-cols-1 sm:grid-cols-2 order-1 m-auto gap-8 sm:gap-6 xl:gap-10 mt-20 sm:mt-0">
@@ -177,8 +177,9 @@ function App() {
                   );
                 })}
               </div>
-              <div className="my-4 hidden xl:block">
-                <Pagination size="large" variant="outlined" page={page} count={Math.ceil(Number(bears?.filter(b => b.name.indexOf(search) !== -1 || search === '').length) / ASSETS_PER_PAGE)} sx={{ color: 'white' }} onChange={(e, v) => setPage(v)} />
+              <div className="my-4">
+                <Pagination className="lg:hidden" size="small" variant="outlined" page={page} count={Math.ceil(Number(bears?.filter(b => b.name.indexOf(search) !== -1 || search === '').length) / ASSETS_PER_PAGE)} sx={{ color: 'white' }} onChange={(e, v) => setPage(v)} />
+                <Pagination className="hidden lg:block" size="large" variant="outlined" page={page} count={Math.ceil(Number(bears?.filter(b => b.name.indexOf(search) !== -1 || search === '').length) / ASSETS_PER_PAGE)} sx={{ color: 'white' }} onChange={(e, v) => setPage(v)} />
               </div>
             </section>
           </TabPanel>
@@ -194,16 +195,16 @@ function App() {
                     Up to 7,000 TEDY
                   </li>
                   <li className="bg-firefly text-center text-[22px] md:text-[30px] xl:text-[50px] 4xl:text-[60px] sm:rounded-[8px] lg:rounded-[15px] rounded 4xl:rounded-[25px] xl:py-10 py-5 md:py-7 4xl:py-[66px] font-bold w-[90%] 4xl:w-[90%] self-end">
-                    +2% FISO Rewards <span className="text-[8px] md:text-[12px] 3xl:text-[16px]">*per NFT</span>
+                    +2% FISO Rewards
                   </li>
                   <li className="bg-firefly text-center text-[22px] md:text-[30px] xl:text-[50px] 4xl:text-[60px] sm:rounded-[8px] lg:rounded-[15px] rounded 4xl:rounded-[25px] xl:py-10 py-5 md:py-7 4xl:py-[66px] font-bold w-[90%] 4xl:w-[90%]">
-                    +0.4% LBE Bonus <span className="text-[8px] md:text-[12px] 3xl:text-[16px]">*per NFT</span>
+                    +0.4% LBE Bonus
                   </li>
                   <li className="bg-firefly text-center text-[22px] md:text-[30px] xl:text-[50px] 4xl:text-[60px] sm:rounded-[8px] lg:rounded-[15px] rounded 4xl:rounded-[25px] xl:py-10 py-5 md:py-7 4xl:py-[66px] font-bold w-[90%] 4xl:w-[90%] self-end">
-                    +0.4% on Yield Farming <span className="text-[8px] md:text-[12px] 3xl:text-[16px]">*per NFT</span>
+                    +0.4% on Yield Farming
                   </li>
                   <li className="bg-firefly text-center text-[22px] md:text-[30px] xl:text-[50px] 4xl:text-[60px] rounded sm:rounded-[8px] lg:rounded-[15px] 4xl:rounded-[25px] py-5 md:py-7 lg:py-10 xl:py-14 4xl:py-[66px] font-bold w-[90%] 4xl:w-[90%]">
-                    +2% Incentivized Testnet <span className="hidden sm:inline-block text-[8px] md:text-[12px] 3xl:text-[16px]">*per NFT</span>
+                    +2% Incentivized Testnet
                   </li>
                 </ul>
               </div>
@@ -216,7 +217,7 @@ function App() {
                     As a member of the Teddy Bears Club in round 2, you have the chance to receive 4,200 to 7,000 TEDY tokens. To learn more, check out our <a target="_blank" className="font-black underline" href="https://docs.teddyswap.org/articles/teddy-bears-club-round-2" rel="noreferrer">article</a>.
                   </p>
                 </div>
-                <ul className="hidden m-0 grow sm:grid grid-cols-1 sm:grid-cols-2 order-1 m-auto gap-8 sm:gap-6 xl:gap-10 mt-20 sm:mt-0">
+                <ul className="hidden m-0 grow sm:grid grid-cols-1 sm:grid-cols-2 order-1 m-auto gap-8 sm:gap-6 xl:gap-10 mt-20 sm:!mb-0">
                   <li className="order-2 lg:order-1"><img src="teddy-1.png" alt="random teddy nft" /></li>
                   <li className="order-3 lg:order-2"><img src="teddy-2.png" alt="random teddy nft" /></li>
                   <li className="order-1 lg:order-3"><img src="teddy-3.png" alt="random teddy nft" /></li>
